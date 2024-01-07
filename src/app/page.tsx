9a7c04a748a78b45e68ec077628791246085b235
metagravity <metagravity0@gmail.com>
2024-01-07 20:34:44 +0100

    .

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -7,7 +7,8 @@ export default function Home() {
   return (
     <MaxWidthWrapper className='mb-12 mt-28 sm:mt-40 flex flex-col items-center justify-center text-center'>
       <div className='space-x-5'>
-        <h1 className="text-center cursor-grab font-semibold text-[102px] dark:text-[#2F2F2F] text-[#B4B4B4]">Bookmq</h1>
+        {/* text-center cursor-grab -[102px]  */}
+        <h1 className="text-zinc-800 text-[114px] dark:text-[#2F2F2F] font-semibold">Bookmq</h1>
         <p className='text-center text-small font-mono'>welcome to{ " " }
           <span className='justify center items-center'>
               <Text className='text-[#E83D22]'>Bookmq</Text>
@@ -19,9 +20,9 @@ export default function Home() {
             type="email"
             placeholder="Email"
             className="w-full px-4 py-2 dark:bg-[#222222] dark:border-[#535353] 
-            ring-[0.3px] dark:ring-[#2B2B2B] dark:text-[#7D7D7D] rounded-sm text-sm 
+            ring-[1px] dark:ring-[#2B2B2B] dark:text-[#959595] rounded-sm text-sm 
             dark:placeholder-[#3F3F3F] bg-[] ring-[#C3C3C3] placeholder-[#2B2B2B]
-            text-[#B5B5B5]"
+            text-[#B5B5B5] "
           />
         </div>
       </div>
--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -15,6 +15,14 @@ const Navbar = () => {
                 </div>
             </MaxWidthWrapper>
         </nav>
+
+        // <nav className="sticky inset-x-12 h-14 w-22 top-5 z-30 border-b rounded-[200px] border-zinc-500 bg-white dark:border-gray-800 dark:bg-white-300/75 backdrop-blur-lg dark:backdrop-blur-lg transition-al">
+        //     <MaxWidthWrapper>
+        //         <div className="flex items-center border-b h-14 justify-between border-zinc-200  dark:border-gray-800">
+        //             <ColorChange />
+        //         </div>
+        //     </MaxWidthWrapper>
+        // </nav>
     )
 }
 
